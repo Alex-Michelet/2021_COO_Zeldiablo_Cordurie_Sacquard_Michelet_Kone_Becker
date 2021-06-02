fd9b08d53167c11dd3146b1c32596be0b25f4c22
MICHELET Alex <alex.michelet7@etu.univ-lorraine.fr>
2021-06-02 11:42:40 +0200

    Ajour constructeur classe Jeu

--- projet_zeldiablo/src/Apps/Jeu.java
+++ projet_zeldiablo/src/Apps/Jeu.java
@@ -8,6 +8,21 @@ public class Jeu{
     private Personnage aventurier;
 
     /**
+     * constructeur parametre qui permet de creer un jeu 
+     * @param p = personnage qui sera l aventurier du jeu
+     * ce constructeur lance une erreur si le personnage donne
+     * n est pas instancie
+     */
+    public Jeu(Personnage p){
+        if(p != null){
+            this.aventurier = p;
+        }
+        else{
+            throw new Error("Un jeu DOIT connaitre un personnage");
+        }
+    }
+
+    /**
      * methode qui permet de deplacer le presonnage du jeu
      * @param dx = deplacement sur l axe x
      * @param dy = deplacement dur l axe y
