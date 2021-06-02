a87e5423e70ce4e15ae993e6680dbd3c6d652728
MICHELET Alex <alex.michelet7@etu.univ-lorraine.fr>
2021-06-02 11:22:40 +0200

    Ajout methode deplacer dans la classe Jeu

--- projet_zeldiablo/src/Apps/Jeu.java
+++ projet_zeldiablo/src/Apps/Jeu.java
@@ -2,6 +2,20 @@ package Apps;
 
 public class Jeu{
 
-    
+    /**
+     * un jeu connait un aventurier
+     */
+    private Personnage aventurier;
+
+    /**
+     * methode qui permet de deplacer le presonnage du jeu
+     * @param dx = deplacement sur l axe x
+     * @param dy = deplacement dur l axe y
+     */
+    public void deplacerPerso(int dx, int dy){
+
+        this.aventurier.deplacer(dx, dy);
+
+    }
 
 }
