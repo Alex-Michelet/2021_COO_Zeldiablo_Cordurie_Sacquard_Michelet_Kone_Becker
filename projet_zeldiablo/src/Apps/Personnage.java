1a839589bd94d4d15716bb5b8a17324895854d68
sacquard1u <julien.sacquard5@etu.univ-lorraine.fr>
2021-06-02 11:20:24 +0200

    classe personnage

--- projet_zeldiablo/src/Apps/Personnage.java
+++ projet_zeldiablo/src/Apps/Personnage.java
@@ -1,5 +1,18 @@
 package Apps;
 
 public class Personnage {
+    private int x, y;
 
+    public Personnage(a, o) {
+        this.x = a;
+        this.y = o;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
 }
