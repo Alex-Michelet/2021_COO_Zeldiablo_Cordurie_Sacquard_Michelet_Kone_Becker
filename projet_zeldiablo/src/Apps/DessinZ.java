b041f56bd27c9902a80fe76b6a858d2447541882
MICHELET Alex <alex.michelet7@etu.univ-lorraine.fr>
2021-06-07 14:36:56 +0200

    Modification DessinZ pour afficher les cases de maniere adaptative en fonction des dimensions de la fenetre

--- projet_zeldiablo/src/Apps/DessinZ.java
+++ projet_zeldiablo/src/Apps/DessinZ.java
@@ -7,7 +7,8 @@ import java.awt.Color;
 
 public class DessinZ implements DessinJeu {
 
-    private int TAILLE_CASE = 20;
+    public int NB_CASES_LONGUEUR = 20;
+    public int NB_CASES_HAUTEUR = 20;
 
     JeuZ jeu;
 
@@ -25,10 +26,22 @@ public class DessinZ implements DessinJeu {
     public void dessiner(BufferedImage image) {
         Graphics2D g = (Graphics2D) image.getGraphics();
         g.setColor(Color.BLUE);
+
+        // on recupere les dimensions de la fenetre
+        int w = image.getWidth();
+        int h = image.getHeight();
+
+        // on defini les dimensions de la cases par rapport a la taille de la fenetre
+        // et le nombre de cases choisi
+        int lon = w/NB_CASES_LONGUEUR;
+        int haut = h/NB_CASES_HAUTEUR;
+
+        // on recupere le personnage du jeu
         Personnage p = jeu.getAventurier();
+        
         // on affiche le personnage en prenant soin de modifier les coordonnees pour s adapter 
         // a la taille des cases du jeu
-        g.fillArc(p.getX() * TAILLE_CASE, p.getY() * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE, 0, 360);
+        g.fillArc(p.getX() * lon, p.getY() * haut, lon, haut, 0, 360);
     }
 
 }
