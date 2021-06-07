45695b76bfa4bb73296e6fa026e2202194d9928e
Cordurie Lucas <lucas.cordurie9@etu.univ-lorraine.fr>
2021-06-07 12:04:20 +0200

    Ajout DessinZ

--- projet_zeldiablo/src/Apps/DessinZ.java
+++ projet_zeldiablo/src/Apps/DessinZ.java
@@ -3,6 +3,7 @@ package Apps;
 import moteurJeu.*;
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
+import java.awt.Color;
 
 public class DessinZ implements DessinJeu {
 
@@ -14,6 +15,7 @@ public class DessinZ implements DessinJeu {
 
     public void dessiner(BufferedImage image) {
         Graphics2D g = (Graphics2D) image.getGraphics();
+        g.setColor(Color.BLUE);
         Personnage p = jeu.getAventurier();
         g.fillArc(p.getX(), p.getY(), 20, 20, 0, 360);
     }
