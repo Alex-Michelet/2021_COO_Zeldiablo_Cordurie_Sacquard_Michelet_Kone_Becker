0c567513eb7d451e5340ae9e05be8454c880fa1f
MICHELET Alex <alex.michelet7@etu.univ-lorraine.fr>
2021-06-07 11:51:54 +0200

    Modification diagramme classe et ajout getter aventurier dans JeuZ

--- projet_zeldiablo/src/Apps/JeuZ.java
+++ projet_zeldiablo/src/Apps/JeuZ.java
@@ -25,42 +25,39 @@ public class JeuZ implements Jeu{
     }
 
     /**
-     * methode qui permet de deplacer le presonnage du jeu
-     * @param dx = deplacement sur l axe x
-     * @param dy = deplacement dur l axe y
-     */
-    public void deplacerPerso(int dx, int dy){
-
-        this.aventurier.deplacer(dx, dy);
-
-    }
-
-    /**
      * methode qui permet de faire evoluer le personnage
      * @param commande = direction du deplacement
      */
     public void evoluer(Commande commande){
         if(commande.bas){
-            this.deplacerPerso(0, 1);
+            this.aventurier.deplacer(commande);
         }
         else if(commande.haut){
-            this.deplacerPerso(0, -1);
+            this.aventurier.deplacer(commande);
         }
         else if(commande.droite){
-            this.deplacerPerso(1, 0);
+            this.aventurier.deplacer(commande);
         }
         else if(commande.gauche){
-            this.deplacerPerso(-1, 0);
+            this.aventurier.deplacer(commande);
         }
     }
 
     /**
      * methode qui permet de savoir si le jeu est fini
      * pour le moment il ne s arrete pas
-     * @return
+     * @return vrai que si le jeu est fini
      */
     public boolean etreFini(){
         return(false);
+    }
+
+    /**
+     * getter aventurier du jeu
+     * @return aventurier du jeu
+     */
+    public Personnage getAventurier(){
+        return(this.aventurier);
     }
 
 }
