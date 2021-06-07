0305c4793c18700f4402649b315d751a39083291
ismaelkone1 <ismael.kone4@etu.univ-lorraine.fr>
2021-06-07 11:04:09 +0200

    Case mise à jour

--- projet_zeldiablo/src/Apps/Case.java
+++ projet_zeldiablo/src/Apps/Case.java
@@ -1,3 +1,27 @@
-public abstract class Case{
-    int 
+public abstract class Case {
+    // Attributs
+    private int caseX;
+    private int caseY;
+    private boolean estFranchissable;
+
+    // Constructeur
+    public Case(int x, int y, boolean f) {
+        this.caseX = x;
+        this.caseY = y;
+        this.estFranchissable = f;
+    }
+
+    // getters
+    public int getCaseX() {
+        return this.caseX;
+    }
+
+    public int getCaseY() {
+        return this.caseY;
+    }
+
+    public boolean estFranchissable() {
+        return this.estFranchissable;
+    }
+
 }
