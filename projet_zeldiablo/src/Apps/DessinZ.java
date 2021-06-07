39553423f049c7800d56855e708f58b05100c11b
Cordurie Lucas <lucas.cordurie9@etu.univ-lorraine.fr>
2021-06-07 15:28:05 +0200

    Modif perso et DessinZ

--- projet_zeldiablo/src/Apps/DessinZ.java
+++ projet_zeldiablo/src/Apps/DessinZ.java
@@ -7,13 +7,11 @@ import java.awt.Color;
 
 public class DessinZ implements DessinJeu {
 
-    public int NB_CASES_LONGUEUR = 20;
-    public int NB_CASES_HAUTEUR = 20;
-
     JeuZ jeu;
 
     /**
      * constructeur qui permet au dessin de connaitre le jeu a afficher
+     * 
      * @param j = jeu a afficher
      */
     public DessinZ(JeuZ j) {
@@ -33,13 +31,14 @@ public class DessinZ implements DessinJeu {
 
         // on defini les dimensions de la cases par rapport a la taille de la fenetre
         // et le nombre de cases choisi
-        int lon = w/NB_CASES_LONGUEUR;
-        int haut = h/NB_CASES_HAUTEUR;
+        int lon = w / jeu.getLabyrinthe().getXLong();
+        int haut = h / jeu.getLabyrinthe().getYLong();
 
         // on recupere le personnage du jeu
         Personnage p = jeu.getAventurier();
-        
-        // on affiche le personnage en prenant soin de modifier les coordonnees pour s adapter 
+
+        // on affiche le personnage en prenant soin de modifier les coordonnees pour s
+        // adapter
         // a la taille des cases du jeu
         g.fillArc(p.getX() * lon, p.getY() * haut, lon, haut, 0, 360);
     }
