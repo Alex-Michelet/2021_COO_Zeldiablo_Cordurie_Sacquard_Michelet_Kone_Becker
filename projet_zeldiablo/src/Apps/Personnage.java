cca5d3e1bbfaf49e1567fcc83c25e754abac63d4
sacquard1u <julien.sacquard5@etu.univ-lorraine.fr>
2021-06-07 11:25:06 +0200

    modif methode deplacer de personnage

--- projet_zeldiablo/src/Apps/Personnage.java
+++ projet_zeldiablo/src/Apps/Personnage.java
@@ -1,11 +1,14 @@
 package Apps;
 
+import moteurJeu.Commande;
+
 public class Personnage {
     /**
      * coordonnees d'un personnage 
      */
     private int x, y;
 
+    final static int LIMIT_X = 15, LIMIT_Y = 15;
     /**
      * Constructeur d'un personnage avec ses coordonnees
      * @param a abscisse
@@ -17,14 +20,38 @@ public class Personnage {
     }
 
     /**
-     * methode qui permet de deplacer le personnage
-     * @param dx deplacement sur l'axe des abscisses
-     * @param dy deplacement sur l'axe des ordonnees
-     */    
-    public void deplacer(int dx, int dy) {
-        this.x+=dx;
-        this.y+=dy;
-    }
+	 * deplacer le personnage dans une direction
+	 * 
+	 * @param c
+	 *            la chaine permettant de deplacer le personnage (N,S, E ou O)
+	 */
+	public void deplacer(Commande c) {
+		
+		if (c.gauche) {
+			this.x--;
+			if (this.x < 0) {
+                this.x = 0;
+            }
+		}
+        if (c.droite) {
+			this.x++;
+			if (this.x >LIMIT_X) {
+                this.x = LIMIT_X;
+            }	
+		}
+        if (c.haut) {
+			this.y++;
+			if (this.x < 0) {
+                this.x = 0;
+            }
+		}
+        if (c.bas) {
+			this.y--;
+			if (this.x < 0) {
+                this.x = 0;
+            }
+		}
+	}
 
     /**
      * methode qui permet d'obtenir la coordonnee x du personnage
