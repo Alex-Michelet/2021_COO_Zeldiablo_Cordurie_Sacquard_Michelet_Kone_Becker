9fcb369f5730bfe3030af20bcb0cc58efcd7aae5
Cordurie Lucas <lucas.cordurie9@etu.univ-lorraine.fr>
2021-06-07 16:59:00 +0200

    correctif Labyrinthe

--- projet_zeldiablo/src/Apps/Labyrinthe.java
+++ projet_zeldiablo/src/Apps/Labyrinthe.java
@@ -2,8 +2,10 @@ package Apps;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.Buffer;
 
 public class Labyrinthe {
@@ -16,35 +18,39 @@ public class Labyrinthe {
      * de celui ci quand il lit un S il cree un sol quand il lit un M il cree un mur
      */
     public Labyrinthe(String nomFichier) {
+        try {
 
-        this.tab = new Case[this.yLong][this.xLong];
-        // Lecteur
-        FileReader lectureDesCases = new FileReader(nomFichier);
-        BufferedReader br = new BufferedReader(lectureDesCases);
-        String line = "";
-        int j;// Iterateur
-        while ((line = br.readLine()) != null) {
-            // Longueur de la ligne
-            this.xLong = line.length();
-            // Nombre de ligne
-            j++;
-        }
-        this.yLong = j;
-        j = 0;
-        char c;
-
-        while ((line = br.readLine()) != null) {
-            for (int i = 0; i < line.length(); i++) {
-                c = line.substring(i, 1);
-                if (c == "S") {
-                    tab[j][i] = new Sol(i, j);
-                } else if (c == "M") {
-                    tab[j][i] = new Mur(i, j);
-                }
+            this.tab = new Case[this.yLong][this.xLong];
+            // Lecteur
+            BufferedReader br = new BufferedReader(new FileReader(nomFichier));
+            String line = "";
+            int j = 0;// Iterateur
+            while ((line = br.readLine()) != null) {
+                // Longueur de la ligne
+                this.xLong = line.length();
+                // Nombre de ligne
+                j++;
             }
-            j++;
-        }
+            this.yLong = j;
+            j = 0;
+            char c;
 
+            while ((line = br.readLine()) != null) {
+                for (int i = 0; i < line.length(); i++) {
+                    c = line.charAt(i);
+                    if (c == 'S') {
+                        tab[j][i] = new Sol(i, j);
+                    } else if (c == 'M') {
+                        tab[j][i] = new Mur(i, j);
+                    }
+                }
+                j++;
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("File not found Exception");
+        } catch (IOException ioe) {
+            System.out.println("IO Exception");
+        }
     }
 
     // getters
@@ -56,7 +62,7 @@ public class Labyrinthe {
         return this.yLong;
     }
 
-    public Labyrinthe getTableauCases() {
-        return Labyrinthe;
+    public Case[][] getTableauCases() {
+        return this.tab;
     }
 }
