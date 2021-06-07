a9c15397d783610861e3a4060b7f450477d276a2
MICHELET Alex <alex.michelet7@etu.univ-lorraine.fr>
2021-06-07 14:25:46 +0200

    Ajout cases Sol et Mur

---
+++ projet_zeldiablo/src/Apps/Mur.java
@@ -0,0 +1,14 @@
+package Apps;
+
+public class Mur extends Case {
+
+    /**
+     * constructeur d un mur qui permet de faire une case non franchissable
+     * @param x = abscisse de la case
+     * @param y = ordonnee de la case
+     */
+    public Mur(int x, int y){
+        super(x, y, false);
+    }
+    
+}
---
+++ projet_zeldiablo/src/Apps/Sol.java
@@ -0,0 +1,14 @@
+package Apps;
+
+public class Sol extends Case {
+
+    /**
+     * constructeur d un sol qui fait une case franchissable
+     * @param x = abscisse de la case
+     * @param y = ordonnee de la case
+     */
+    public Sol(int x, int y){
+        super(x, y, true);
+    }
+    
+}
