2c6d80b8d46d43479747e1d81b5a6ffc651e1465
sacquard1u <julien.sacquard5@etu.univ-lorraine.fr>
2021-06-07 17:11:09 +0200

    ajout TestJeuZ

---
+++ projet_zeldiablo/src/Tests/TestJeuZ.java
@@ -0,0 +1,42 @@
+package Tests;
+
+import Apps.*;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import moteurJeu.Commande;
+
+public class testJeuZ {
+    /**
+     * test de la construction d'un jeu
+     */
+    @Test 
+    public void testCreaJeuZ() {
+        // prepa des donnees 
+        Personnage p = new Personnage(1,1);
+        Labyrinthe l = new Labyrinthe("../../Niveaux/lab1.txt");
+        JeuZ j = new JeuZ(p, l);
+
+        // verif des donnees
+        assertEquals("Il devrait y avoir un personnage", p, j.getAventurier());
+        assertEquals("Il devrait y avoir un labyrinthe", l, j.getLabyrinthe());
+    }
+
+    /**
+     * test deplacement dans un labyrinthe
+     */
+    @Test 
+    public void testEvoluer() {
+        // prepa des donnees
+        Personnage p = new Personnage(1,1);
+        Labyrinthe l = new Labyrinthe("../../Niveaux/lab1.txt");
+        JeuZ j = new JeuZ(p, l);
+        Commande c = new Commande();
+        //methode teste 
+        j.evoluer(c.bas);
+        //verif des donnees
+        assertEquals("x devrait etre à 1", 1, j.getAventurier().getX());
+        assertEquals("y devrait etre à 2", 2, j.getAventurier().getY());
+    }
+
+}
