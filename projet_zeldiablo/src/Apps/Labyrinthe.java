48afa9e38e55a4b26546b82d1dd173e0e1275494
ismaelkone1 <ismael.kone4@etu.univ-lorraine.fr>
2021-06-07 17:31:45 +0200

    Labyrinthe MAJ correction

--- projet_zeldiablo/src/Apps/Labyrinthe.java
+++ projet_zeldiablo/src/Apps/Labyrinthe.java
@@ -1,10 +1,8 @@
 package Apps;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.Buffer;
 
@@ -16,26 +14,31 @@ public class Labyrinthe {
      * Constructeur d un Labyrinthe Un labyrinthe est compose de cases de types :
      * sol et mur Un labyrinthe lit un fichier texte et cree les cases en fonction
      * de celui ci quand il lit un S il cree un sol quand il lit un M il cree un mur
+     * 
+     * @param nomFichier
      */
     public Labyrinthe(String nomFichier) {
         try {
 
-            this.tab = new Case[this.yLong][this.xLong];
-            // Lecteur
-            BufferedReader br = new BufferedReader(new FileReader(nomFichier));
+            BufferedReader br = new BufferedReader(new FileReader(nomFichier)); // Lecteur
             String line = "";
             int j = 0;// Iterateur
-            while ((line = br.readLine()) != null) {
+
+            br.mark(0);
+            while ((line = br.readLine()) != null) { // On parcourt le fichier pour connaître la taille du labyrinthe
                 // Longueur de la ligne
                 this.xLong = line.length();
                 // Nombre de ligne
                 j++;
             }
             this.yLong = j;
+            this.tab = new Case[this.yLong][this.xLong];
+
             j = 0;
             char c;
 
-            while ((line = br.readLine()) != null) {
+            br.reset();
+            while ((line = br.readLine()) != null) { // On parcourt le fichier pour creer les cases correspondantes
                 for (int i = 0; i < line.length(); i++) {
                     c = line.charAt(i);
                     if (c == 'S') {
@@ -46,6 +49,7 @@ public class Labyrinthe {
                 }
                 j++;
             }
+            br.close();
         } catch (FileNotFoundException fnfe) {
             System.out.println("File not found Exception");
         } catch (IOException ioe) {
@@ -53,18 +57,39 @@ public class Labyrinthe {
         }
     }
 
-    // getters
+    /**
+     * getTailleX
+     * 
+     * @return xLong
+     */
     public int getTailleX() {
         return this.xLong;
     }
+
+    /**
+     * getTailleY
+     * 
+     * @return yLong
+     */
 
     public int getTailleY() {
         return this.yLong;
     }
 
+    /**
+     * getTableauCase
+     * 
+     * @return tab
+     */
     public Case[][] getTableauCases() {
         return this.tab;
     }
+
+    /**
+     * getTailleX
+     * 
+     * @return booleen indiquant si la case du labyrinthe est franchissable
+     */
 
     public boolean estAccessible(int x, int y) {
         return (tab[y][x].estFranchissable());
