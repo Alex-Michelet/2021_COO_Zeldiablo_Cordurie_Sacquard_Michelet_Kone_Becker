6711a5e9a1e045dcab63e5c198ede27a7f83995c
MICHELET Alex <alex.michelet7@etu.univ-lorraine.fr>
2021-06-08 10:51:11 +0200

    Nouveau squelette methode evoluer dans le jeu qui prend en compte les pieges du labyrinthe

--- projet_zeldiablo/src/Apps/JeuZ.java
+++ projet_zeldiablo/src/Apps/JeuZ.java
@@ -36,31 +36,77 @@ public class JeuZ implements Jeu{
      * @param commande = direction du deplacement
      */
     public void evoluer(Commande commande){
+
+        int xPersoAncien = this.aventurier.getX();
+        int yPersoAncien = this.aventurier.getY();
+
+        // si le perso c est bien deplace on gere les pieges
+        if(this.deplacerPerso(commande)){
+
+            // on verifie si le personnage etait sur un piege avant de bouger
+            if(this.labyrinthe.estUnPiege(xPersoAncien, yPersoAncien)){
+
+                // si c etait le cas on reactive le piege
+                this.labyrinthe.activerPiege(xPersoAncien, yPersoAncien);
+            }
+
+            // on recupere les coordonnees actuelles du personnage
+            int xPersoActuel = this.aventurier.getX();
+            int yPersoActuel = this.aventurier.getY();
+
+            // on regarde si le personnage est arrive sur un piege
+            if(this.labyrinthe.estUnPiege(xPersoActuel, yPersoActuel)){
+
+                // si on est bien sur un piege on s assure qu il est actif
+                if(this.labyrinthe.getEtatPiege(xPersoActuel, yPersoActuel)){
+
+                    // on fait subir des degats au personnage
+                    this.aventurier.prendreDegats(1);
+
+                    // puis on pense a desactiver le piege
+                    this.labyrinthe.desactiverPiege(xPersoActuel, yPersoActuel);
+                }
+            }
+        }
+
+    }
+    
+    public boolean deplacerPerso(Commande commande){
+
+        boolean res = false;
+
         // on recupere les coordonnees de l aventurier
         int xPerso = this.aventurier.getX();
         int yPerso = this.aventurier.getY();
-
+    
         // on verifie que l aventurier puisse bien aller a la case souhaitee
         if (commande.gauche) {
-			if (xPerso > 0 && this.labyrinthe.estAccessible(xPerso - 1, yPerso)) {
+            if (xPerso > 0 && this.labyrinthe.estAccessible(xPerso - 1, yPerso)) {
                 this.aventurier.deplacer(-1, 0);
             }
-		}
+        }
         if (commande.droite) {
-			if (xPerso < this.labyrinthe.getTailleX() - 1 && this.labyrinthe.estAccessible(xPerso + 1, yPerso)) {
+            if (xPerso < this.labyrinthe.getTailleX() - 1 && this.labyrinthe.estAccessible(xPerso + 1, yPerso)) {
                 this.aventurier.deplacer(1, 0);
             }	
-		}
+        }
         if (commande.haut) {
-			if (yPerso > 0 && this.labyrinthe.estAccessible(xPerso, yPerso - 1)) {
+            if (yPerso > 0 && this.labyrinthe.estAccessible(xPerso, yPerso - 1)) {
                 this.aventurier.deplacer(0, -1);
             }
-		}
+        }
         if (commande.bas) {
-			if (yPerso < this.labyrinthe.getTailleY() - 1 && this.labyrinthe.estAccessible(xPerso, yPerso + 1)) {
+            if (yPerso < this.labyrinthe.getTailleY() - 1 && this.labyrinthe.estAccessible(xPerso, yPerso + 1)) {
                 this.aventurier.deplacer(0, 1);
             }
-		}
+        }
+
+        // on verifie que le deplacement a bien eu lieu
+        if(xPerso != this.aventurier.getX() || yPerso != this.aventurier.getY()){
+            res = true;
+        }
+        
+        return(res);
     }
 
     /**
