0fbda795398827c49fb093d88a8ceb697a082453
ismaelkone1 <ismael.kone4@etu.univ-lorraine.fr>
2021-06-08 10:30:08 +0200

    Test MAJ, cas d'un piege cree

--- projet_zeldiablo/src/Tests/TestLabyrinthe.java
+++ projet_zeldiablo/src/Tests/TestLabyrinthe.java
@@ -5,20 +5,20 @@ import Apps.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-
 public class TestLabyrinthe {
 
     /**
      * test de la construction d un labyrinthe simple
      */
     @Test
-    public void testCreaLabyrinthe(){
-        //prepa des donnees
+    public void testCreaLabyrinthe() {
+        // prepa des donnees
         Labyrinthe l = new Labyrinthe("niveaux/lab1.txt");
 
-        //verification des donnees
+        // verification des donnees
         assertEquals("La case devrait etre un mur donc inaccessible", false, l.estAccessible(0, 0));
         assertEquals("La case devrait etre un sol donc accessible", true, l.estAccessible(1, 1));
+        assertEquals("La case devrait etre un piege donc accessible", true, l.estAccessible(2, 2));
     }
-    
+
 }
