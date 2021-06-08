222062e41914cdd3077aa325b22717afe833ff79
ismaelkone1 <ismael.kone4@etu.univ-lorraine.fr>
2021-06-08 16:52:43 +0200

    Arme MAJ javadoc

--- projet_zeldiablo/src/Apps/Arme.java
+++ projet_zeldiablo/src/Apps/Arme.java
@@ -15,6 +15,12 @@ public abstract class Arme {
         this.portee = p;
     }
 
+    /**
+     * attaquer applique la methode subir degat d une entite et retire le nombre de
+     * degats correspondant a l arme utilisee
+     * 
+     * @param e
+     */
     public void attaquer(Entite e) {
         e.subirDegats(this.degat);
     }
