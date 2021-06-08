872a7e53d44f4de92c508c9c37764abd58689f36
Cordurie Lucas <lucas.cordurie9@etu.univ-lorraine.fr>
2021-06-08 15:53:15 +0200

    modif monstre et troll

---
+++ projet_zeldiablo/src/Apps/Troll.java
@@ -0,0 +1,15 @@
+package Apps;
+
+public class Troll extends Monstre {
+
+    /**
+     * Constructeur de Troll
+     * 
+     * @param a abscisse
+     * @param o ordonnee
+     */
+    public Troll(int a, int o) {
+        super(a, o, 5, 3);
+    }
+
+}
