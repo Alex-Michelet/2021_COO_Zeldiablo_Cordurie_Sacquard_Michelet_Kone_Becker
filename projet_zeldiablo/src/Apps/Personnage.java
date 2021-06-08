3930a891a9d0842a5d1a01045ab0c41be5722add
Cordurie Lucas <lucas.cordurie9@etu.univ-lorraine.fr>
2021-06-08 09:56:53 +0200

    fusion personnage

--- projet_zeldiablo/src/Apps/Personnage.java
+++ projet_zeldiablo/src/Apps/Personnage.java
@@ -1,11 +1,14 @@
 package Apps;
 
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Personnage {
     /**
      * coordonnees d'un personnage
      */
     private int posX, posY;
+    private boolean invincible;
 
     /**
      * Constructeur d'un personnage avec ses coordonnees
@@ -16,6 +19,7 @@ public class Personnage {
     public Personnage(int a, int o) {
         this.posX = a;
         this.posY = o;
+        this.invincible = false;
     }
 
     /**
@@ -26,6 +30,30 @@ public class Personnage {
     public void deplacer(int x, int y) {
         this.posX += x;
         this.posY += y;
+    }
+
+    public void prendreDegat(int deg) {
+        int val = Math.abs(deg);
+        if ((!invincible) && (!etreMort())) {
+            invincible = true;
+            Timer t = new Timer();
+            TimerTask tt = new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("Invulnerable BRAVO");
+                    Thread.sleep(5 * 1000);
+                    System.out.println("attention aie aie aie etc");
+                    invincible = false;
+                };
+            };
+            if (pv < val) {
+                pv = 0;
+            } else {
+                pv -= val;
+            }
+            System.out.println("les degats sont pris : pv = " + getPv());
+
+        }
     }
 
     /**
