d47cd8c13805b62cf3ca7779c2ef067ea29d4e61
MICHELET Alex <alex.michelet7@etu.univ-lorraine.fr>
2021-06-08 16:02:35 +0200

    Ajout classes Epee et Lance

---
+++ projet_zeldiablo/src/Apps/Lance.java
@@ -0,0 +1,13 @@
+package Apps;
+
+public class Lance extends Arme {
+
+    /**
+     * constructeur d un epee qui a 
+     * 1 de portee et 2 de degats
+     */
+    public Lance(){
+        super(2, 2);
+    }
+    
+}
