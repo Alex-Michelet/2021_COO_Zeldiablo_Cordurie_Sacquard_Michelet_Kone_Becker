e520d880a50b1f61786cf33b686764d1e018bc87
MICHELET Alex <alex.michelet7@etu.univ-lorraine.fr>
2021-06-11 11:54:35 +0200

    Complements de tests sur le jeu

--- projet_zeldiablo/src/Tests/TestJeuZ.java
+++ projet_zeldiablo/src/Tests/TestJeuZ.java
@@ -62,4 +62,80 @@ public class TestJeuZ {
 
     }
 
+    /**
+     * test d un deplacement du perso sur un monstre
+     * le perso ne doit pas bouger
+     * il y a un montre en 1, 2
+     */
+    @Test
+    public void testDeplacementSurEntite(){
+        //prepa des donnees 
+        Personnage p = new Personnage(1,1, 15);
+        Labyrinthe l = new Labyrinthe("niveaux/lab1.txt");
+        JeuZ j = new JeuZ(p, l);
+        Commande c = new Commande();
+        c.bas = true;
+        //methode teste 
+        j.evoluer(c);
+
+        //verif des donnees
+        assertEquals("x devrait rester à 1", 1, j.getAventurier().getX());
+        assertEquals("y devrait etre à 1", 1, j.getAventurier().getY());
+    }
+
+    /**
+     * test de la prise d arme dans le labyrinthe
+     */
+    @Test
+    public void testPriseArme(){
+        //prepa des donnees 
+        Personnage p = new Personnage(8,1, 15);
+        Labyrinthe l = new Labyrinthe("niveaux/lab2.txt");
+        JeuZ j = new JeuZ(p, l);
+        Commande c = new Commande();
+        c.prendreArme = true;
+        //methode teste 
+        j.evoluer(c);
+
+        //verif des donnees
+        assertEquals("le perso devrait avoir une lance", true, p.getArme() instanceof Lance);
+        assertEquals("La case devrait etre vide", null, l.getArmeCase(8, 1));
+    }
+
+    /**
+     * test depose d arme quand le perso en a une
+     * la case doit avoir l arme et le perso plus rien
+     */
+    @Test
+    public void testDeposeArme(){
+        //prepa des donnees 
+        Personnage p = new Personnage(8,1, 15);
+        Labyrinthe l = new Labyrinthe("niveaux/lab2.txt");
+        JeuZ j = new JeuZ(p, l);
+        Commande c = new Commande();
+        c.prendreArme = true;
+
+        //methode teste 
+        j.evoluer(c);
+
+        // premiere verif
+        assertEquals("le perso devrait avoir une lance", true, p.getArme() instanceof Lance);
+        assertEquals("La case devrait etre vide", null, l.getArmeCase(8, 1));
+
+        // on est oblige d attendre un peu avant de relancer une commande
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
+            System.out.println("Probleme d attente");
+        }
+
+        // relance de la methode
+        c.prendreArme = true;
+        j.evoluer(c);
+
+        //verif des donnees
+        assertEquals("le perso ne devrait pas etre arme", null, p.getArme());
+        assertEquals("le case devrait avoir une lance", true, l.getArmeCase(8, 1) instanceof Lance);
+    }
+
 }
