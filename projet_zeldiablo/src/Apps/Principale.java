2e301f210f26805bf2d4dfb7ba23f644952334e2
MICHELET Alex <alex.michelet7@etu.univ-lorraine.fr>
2021-06-15 10:08:19 +0200

    Ajout d armes et de pieges dans le nouveau niveau

--- projet_zeldiablo/src/Apps/Principale.java
+++ projet_zeldiablo/src/Apps/Principale.java
@@ -11,13 +11,17 @@ public class Principale {
      */
     public static void main(String[] args) throws Exception {
 
+        // on prepare les donnees du jeu
         Labyrinthe labyrinthe = new Labyrinthe("niveaux/lab3.txt");
         JeuZ jeu = new JeuZ(labyrinthe);
 
+        // on prepare l interface graphique
         DessinZ dessin = new DessinZ(jeu);
 
+        // on prepare le moteur graphique
         MoteurGraphique moteur = new MoteurGraphique(jeu, dessin);
 
+        // on lance le moteur de jeu
         moteur.lancerJeu(700, 700);
     }
 
